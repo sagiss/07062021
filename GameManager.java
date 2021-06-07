db39668dda53b5364b8e22fbd5db02ac927a003a
sagiss <85236283+sagiss@users.noreply.github.com>
2021-06-07 19:21:39 +0300

    Add files via upload

---
+++ GameManager.java
@@ -0,0 +1,99 @@
+import java.util.*;
+
+public class GameManager {
+    private final ArrayList<Integer> lines;
+    private int turn; //0- p1 1 -p2
+
+    public GameManager(int lines) {
+        int numOfMaklot = 1; // reset lines in pyramid shape
+        this.lines = new ArrayList<Integer>();
+        for (int i = 0; i < lines; i++) {
+            this.lines.add(numOfMaklot);
+            numOfMaklot += 2;
+        }
+        Random rand = new Random();
+        this.turn = rand.nextInt(2);
+    }
+
+    public GameManager(GameManager other) {
+        this.lines = new ArrayList<Integer>();
+        this.lines.addAll(other.lines);
+        this.turn = other.turn;
+    }
+
+    public boolean playTurn(int selectedRow, int amount) {
+        if (this.lines.get(selectedRow) >= amount) {
+            this.lines.set(selectedRow,this.lines.get(selectedRow)-amount);
+        } else {
+            return false;
+        }
+
+        if (getWinner().equals("none")) {
+            if (this.turn == 0) {
+                this.turn = 1;
+            } else {
+                this.turn = 0;
+
+            }
+        }
+        return true;
+
+    }
+
+    public void addNumToLine(int line,int amount){
+        lines.set(line,lines.get(line)+amount);
+    }
+
+    public String getWinner() {
+        int count = 0;
+        for (int i = 0; i < lines.size(); i++) {
+            if (!(lines.get(i) == 1) && !(lines.get(i) == 0))
+                return "none";
+            if (lines.get(i) == 1)
+                count++;
+        }
+        if (count == 1) {
+            return turn == 0 ? "ai" : "player";
+        }
+        return "none";
+    }
+
+    public boolean isLegalBoard(){
+        int sum =0;
+        for (Integer i:lines) {
+            sum+=i;
+        }
+        return sum != 0;
+    }
+
+    public int getTurn() {
+        return turn;
+    }
+
+    public ArrayList<Integer> getLines() {
+        return lines;
+    }
+
+
+    public void print() {
+        for (int i = 0; i < lines.size(); i++) {
+            Integer m = lines.get(i);
+            System.out.print(i + 1 + "  ");
+            for (int j = 0; j < m; j++) {
+                System.out.print("X");
+            }
+            System.out.println();
+        }
+    }
+
+//    public String getBoardGraphics(){
+//        StringBuilder output= new StringBuilder();
+//        for (int i = 0; i < lines.size(); i++) {
+//            Integer m = lines.get(i);
+//            output.append(i+1).append("  ");
+//            output.append("X".repeat(Math.max(0, m)));
+//            output.append("\n");
+//        }
+//        return output.toString();
+//    }
+}
---
+++ Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        GameManager gm = new GameManager(4);
+        gm.print();
+        Ai ai = new Ai();
+
+        Scanner sc = new Scanner(System.in);
+
+        while (gm.getWinner().equals("none")) {
+            if (gm.getTurn() == 0) {
+                System.out.println("AI");
+                Move move = ai.playTurn(gm);
+                gm.playTurn(move.row,move.amount);
+            } else {
+                System.out.println("player " + (gm.getTurn() + 1) + " its your turn");
+                System.out.println("choose row");
+                int row = Integer.parseInt(sc.nextLine());
+                row--;
+                System.out.println("choose amount");
+                int amount = Integer.parseInt(sc.nextLine());
+                if (!gm.playTurn(row, amount)){
+                    System.out.println("AGAIN");
+                }
+            }
+            gm.print();
+            System.out.println("\n");
+
+        }
+        System.out.println("Winner " + gm.getWinner());
+    }
+}
